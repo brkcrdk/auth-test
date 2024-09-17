8ae842963e9c955e2685c63c34c890422f353d48
elinoza <hillcakmak@gmail.com>
2024-09-17 17:58:10 +0300

    defaultMutation

--- services/defaultMutation.ts
+++ services/defaultMutation.ts
@@ -1,2 +1,35 @@
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-
+export default async function mutation(
+  endpoint: string,
+  requestParams: RequestInit
+) {
+  const token = cookies().get("token")?.value;
+  const refreshToken = cookies().get("refreshToken")?.value;
+  if (!token) {
+    redirect("/logout");
+  }
+  const computedRequestParam: RequestInit = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      ...requestParams.headers,
+    },
+  };
+  if (requestParams.body) {
+    let data = JSON.stringify(requestParams.body);
+    computedRequestParam.body = data;
+  }
+  const response = await fetch(
+    `${process.env.BASE_URL}${endpoint}`,
+    computedRequestParam
+  );
+  if (response.status === 401) {
+    redirect("/logout");
+  }
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+}
