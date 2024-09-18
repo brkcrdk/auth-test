faea278b19b0c1ecc772decc322624401f689cf8
elinoza <hillcakmak@gmail.com>
2024-09-19 01:07:14 +0300

    some fixes on defaultMutation.ts

--- services/defaultMutation.ts
+++ services/defaultMutation.ts
@@ -1,35 +1,41 @@
+"use server";
+
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
-export default async function mutation(
-  endpoint: string,
-  requestParams: RequestInit
-) {
+export async function mutation(endpoint: string, requestParams: RequestInit) {
   const token = cookies().get("token")?.value;
-  const refreshToken = cookies().get("refreshToken")?.value;
+
   if (!token) {
     redirect("/logout");
   }
-  const computedRequestParam: RequestInit = {
-    method: "POST",
+
+  const computedRequestParams: RequestInit = {
+    method: requestParams.method || "POST",
     headers: {
       Authorization: `Bearer ${token}`,
       ...requestParams.headers,
     },
   };
+
   if (requestParams.body) {
-    let data = JSON.stringify(requestParams.body);
-    computedRequestParam.body = data;
+    const data = JSON.stringify(requestParams.body);
+    computedRequestParams.body = data;
   }
+
   const response = await fetch(
     `${process.env.BASE_URL}${endpoint}`,
-    computedRequestParam
+    computedRequestParams
   );
+
   if (response.status === 401) {
     redirect("/logout");
   }
-  if (response.ok) {
-    const data = await response.json();
-    return data;
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status ${response.status}`);
   }
+
+  const data = await response.json();
+  return data;
 }
