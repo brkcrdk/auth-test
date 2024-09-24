aba28eee7c205d7296a8b145ebc2efbb00e3d251
aycanogut <aycanonfire@gmail.com>
2024-09-24 17:57:46 +0300

    UPDATE: add protected route

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,53 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import authTestConfig from "./authTestConfig";
+
+interface RefreshResponseProps {
+  accessToken: string;
+  refreshToken: string;
+}
+
 export async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith("/logout")) {
     const res = NextResponse.redirect(new URL("/login", req.url));
-    res.cookies.delete("token");
-    res.cookies.delete("refreshToken");
+    res.cookies.delete(authTestConfig.accessToken);
+    res.cookies.delete(authTestConfig.refreshToken);
     return res;
-  } else {
+  }
+  else if (req.nextUrl.pathname.startsWith("/protected")) {
+    const token = req.cookies.get(authTestConfig.accessToken)?.value;
+    const refreshToken = req.cookies.get(authTestConfig.refreshToken)?.value;
+
+    if (!refreshToken && !token) {
+      return NextResponse.redirect(new URL("/logout", req.url));
+    }
+
+    const refreshRequest = await fetch(`${process.env.BASE_URL}/auth/refresh`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        refreshToken: refreshToken,
+        expiresInMins: 30,
+      }),
+      credentials: "include",
+    })
+
+    if (refreshRequest.ok) {
+      const refreshResponse: RefreshResponseProps = await refreshRequest.json();
+      const res = NextResponse.next();
+      res.cookies.set(authTestConfig.accessToken, refreshResponse.accessToken);
+      res.cookies.set(authTestConfig.refreshToken, refreshResponse.refreshToken);
+      return res;
+    }
+    else {
+      return NextResponse.redirect(new URL("/logout", req.url));
+    }
+  }
+  else {
     return NextResponse.next();
   }
 }
+
 export const config = {
   matcher: [
     /*
